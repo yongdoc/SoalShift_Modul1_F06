5993fa98a935b100bd49b2bdd55b0b650d3b77d2
yongdoc <47826077+yongdoc@users.noreply.github.com>
2019-02-22 17:00:39 +0700

    Update soal5.sh

--- soal5.sh
+++ soal5.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-awk 'tolower($0) ~ /cron/,!/sudo/ {if(NF<=12)print}' /var/log/syslog >> ~/modul1/user.log
+awk 'tolower($0) ~ !/sudo/,/cron/ {if(NF<=12)print}' /var/log/syslog >> ~/user.log
